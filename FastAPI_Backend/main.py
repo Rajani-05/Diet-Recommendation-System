a7096a5ff49ddaed2179e833c35d4a20840ceafc
Beerangi Rajani <rajani.coding@gmail.com>
2026-03-11 23:18:00 +0530

    Update main.py

--- FastAPI_Backend/main.py
+++ FastAPI_Backend/main.py
@@ -1,41 +1,42 @@
 from fastapi import FastAPI
-from pydantic import BaseModel,conlist
-from typing import List,Optional
+from pydantic import BaseModel, conlist
+from typing import List, Optional
 import pandas as pd
-from model import recommend,output_recommended_recipes
+from model import recommend, output_recommended_recipes
 
-
-dataset=pd.read_csv('../Data/dataset.csv',compression='gzip')
+# Correct dataset path
+dataset = pd.read_csv("../Data/dataset.csv", compression="gzip")
 
 app = FastAPI()
 
+class Params(BaseModel):
+    n_neighbors: int = 5
+    return_distance: bool = False
 
-class params(BaseModel):
-    n_neighbors:int=5
-    return_distance:bool=False
 
 class PredictionIn(BaseModel):
-    nutrition_input:conlist(float, min_items=9, max_items=9)
-    ingredients:list[str]=[]
-    params:Optional[params]
+    nutrition_input: conlist(float, min_items=9, max_items=9)
+    ingredients: List[str] = []
+    params: Optional[Params]
 
 
 class Recipe(BaseModel):
-    Name:str
-    CookTime:str
-    PrepTime:str
-    TotalTime:str
-    RecipeIngredientParts:list[str]
-    Calories:float
-    FatContent:float
-    SaturatedFatContent:float
-    CholesterolContent:float
-    SodiumContent:float
-    CarbohydrateContent:float
-    FiberContent:float
-    SugarContent:float
-    ProteinContent:float
-    RecipeInstructions:list[str]
+    Name: str
+    CookTime: str
+    PrepTime: str
+    TotalTime: str
+    RecipeIngredientParts: List[str]
+    Calories: float
+    FatContent: float
+    SaturatedFatContent: float
+    CholesterolContent: float
+    SodiumContent: float
+    CarbohydrateContent: float
+    FiberContent: float
+    SugarContent: float
+    ProteinContent: float
+    RecipeInstructions: List[str]
+
 
 class PredictionOut(BaseModel):
     output: Optional[List[Recipe]] = None
@@ -46,12 +47,19 @@ def home():
     return {"health_check": "OK"}
 
 
-@app.post("/predict/",response_model=PredictionOut)
-def update_item(prediction_input:PredictionIn):
-    recommendation_dataframe=recommend(dataset,prediction_input.nutrition_input,prediction_input.ingredients,prediction_input.params.dict())
-    output=output_recommended_recipes(recommendation_dataframe)
-    if output is None:
-        return {"output":None}
-    else:
-        return {"output":output}
+@app.post("/predict/", response_model=PredictionOut)
+def predict(prediction_input: PredictionIn):
 
+    recommendation_dataframe = recommend(
+        dataset,
+        prediction_input.nutrition_input,
+        prediction_input.ingredients,
+        prediction_input.params.dict()
+    )
+
+    output = output_recommended_recipes(recommendation_dataframe)
+
+    if output is None:
+        return {"output": None}
+    else:
+        return {"output": output}
